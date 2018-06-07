9209c9f6df7e9fed9773d74b809c83f77e4dfa23
Logan Call <this@logancall.com>
2018-06-07 08:26:16 -0600

    Start development

---
+++ src/MegaMenu/index.js
@@ -0,0 +1,10 @@
+// @flow
+import React from 'react'
+//
+import Styled from './index.style'
+
+export default class MegaMenu extends React.Component {
+  render() {
+    return <span>hi</span>
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Hello from './Hello'
+import faker from 'faker'
+import _ from 'lodash'
+//
+import MegaMenu from './MegaMenu'
 
 const styles = {
   fontFamily: 'sans-serif',
   textAlign: 'center',
 }
 
+let menuItems = []
+let count = 6
+
+while (count > 0) {
+  menuItems.push({
+    id: faker.random.uuid(),
+    title: faker.lorem.word(),
+    url: count % 2 ? '' : faker.internet.url(),
+    sections: _.times(faker.random.number(5), () => ({
+      id: faker.random.uuid(),
+      title: faker.lorem.word(),
+      url: count % 2 ? '' : faker.internet.url(),
+      items: _.times(faker.random.number(8), () => ({
+        id: faker.random.uuid(),
+        title: faker.lorem.word(),
+        url: faker.internet.url(),
+      })),
+    })),
+  })
+  count -= 1
+}
+
+const menuStyles = {}
+
 const App = () => (
   <div style={styles}>
-    <Hello name="CodeSandbox" />
-    <h2>Start editing to see some magic happen {'\u2728'}</h2>
+    <MegaMenu
+      options={{
+        items: menuItems,
+        styles: menuStyles,
+      }}
+    />
   </div>
 )
 
